a640a566bfe70dc4717224735e030a7e291fc7f2
Smit Patel <smitp3346@gmail.com>
2026-05-01 22:04:52 +0530

    updated changes

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 board = [""] * 9
+game_over = False
+
 
 def check_winner(player):
     wins = [
@@ -15,21 +17,32 @@ def check_winner(player):
             return True
     return False
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/game")
 def game():
-    global board
+    global board, game_over
     board = [""] * 9
+    game_over = False
     return render_template("game.html")
+
 
 @app.route("/move", methods=["POST"])
 def move():
-    global board
+    global board, game_over
+
+    if game_over:
+        return jsonify({
+            "board": board,
+            "winner": None,
+            "draw": False
+        })
+
     data = request.get_json()
-    
     index = data["index"]
     player = data["player"]
 
@@ -39,11 +52,15 @@ def move():
     winner = check_winner(player)
     draw = "" not in board and not winner
 
+    if winner:
+        game_over = True
+
     return jsonify({
         "board": board,
         "winner": player if winner else None,
         "draw": draw
     })
+
 
 if __name__ == "__main__":
     app.run(debug=True)
